58e4ec6215bfca13012679d55849c9d829859177
Waheeb_Alhadi <157744295+Waheebalhadi@users.noreply.github.com>
2025-11-16 21:41:31 +0300

    Delete src directory

--- src/components/TopBar.tsx
+++
@@ -1,7 +0,0 @@
-export const TopBar = () => {
-  return (
-    <div className="bg-[hsl(var(--top-bar))] text-[hsl(var(--top-bar-foreground))] py-2 text-center text-sm">
-      <p>✨ جودة عالية • ألوان متنوعة • خدمة متميزة • استشارات مجانية ✨</p>
-    </div>
-  );
-};
